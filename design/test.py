b256b5dd862df7e4a09184ceb9cffedc6e5c17fc
Navi <lunaqua@proton.me>
2024-10-03 21:49:24 +0100

    Cleanned up the code.
    Added comments etc..

--- design/test.py
+++ design/test.py
@@ -1,27 +1,29 @@
 #!/bin/python
 
 import requests as r
-
-#url = "https://ct.wiimm.de/ajax.php?ajax=ctt"
-url = "https://ct.wiimm.de/index?ajax=ctt"
-header = {"Content-Type": "application/x-www-form-urlencoded", "Cookie": "CT_WIIMM_DE_SESSION24=3645888-Zv7L5ABJLAJaHtLLQAPJyWBU"}
-
-ref = int(input("Enter a track ref: "))
-payload = {"seq": "1", "j": "tooltip", "id": "ct-tooltip", "ref": ref}
-
-# req = r.post(url, headers=header, data=payload)
-# print(req.status_code)
-# print(req.text)
-
-# url = "https://ct.wiimm.de/ajax.php?ajax=ctt&seq=1&j=tooltip&id=ct-tooltip&ref=6841"
-# 
-# req = r.get(url, headers=header)
-# print(req.status_code)
-# print(req.content)
+# Used to download the information page for the track
+from bs4 import BeautifulSoup as bs
+# Used to decode the html table returned by the site
+import simplejson as j
+# Used to extract the html body from the json returned
 
 url = "https://ct.wiimm.de/ajax/get-info"
+# get-info is a php script.
+# used over index.php?ajax=ctt, as get-info contains more information.
+# also used over i/[id], as parsing get-info is easier.
+header = {"Content-Type": "application/x-www-form-urlencoded", "Cookie": "CT_WIIMM_DE_SESSION24=3645888-Zv7L5ABJLAJaHtLLQAPJyWBU"}
+# required cookie, should be periodically refreshed.
+
+ref = int(input("Enter a track ref: "))
 payload = {"f": ref}
+# Sets up the required data for the script.
 
 req = r.post(url, headers=header, data=payload)
-print(req.status_code)
-print(req.text)
+# Sends the post request to the URL, and saves the response. should really
+#   check the returned status.
+htmlsoup = bs(j.loads(req.text)["modal"]["body"], "html.parser")
+# Creates a new bs object from the returned decoded json, under modal, then
+#   body keys.
+
+print(htmlsoup.prettify())
+# prints a new looking variation of the html.
