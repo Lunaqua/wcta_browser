38c08ab2276e1e3fd894fb644bd3fda076b316f6
Navi <lunaqua@proton.me>
2024-10-17 20:10:47 +0100

    Added settings, and cookie retrieval, including error checking.
    Code can absolutely be cleaned up slightly, but for now this works.

--- main.py
+++ main.py
@@ -28,9 +28,16 @@ import loadspin as ls
 # Used to display a fancy little loading animation
 import argparse as ap
 # ArgumentParser
+import requests as r
+# Used to make http requests
+import simplejson as j
+# Used to manage settings.
+import time
+# Used to provide the time for checking cookie expiration
 
 __VERSION__ = "0.0.1"
 __SETTINGS__ = "settings.json"
+__INDEXURL__ = "https://ct.wiimm.de/index"
 # Define basic information
 
 def arginit():
@@ -42,11 +49,41 @@ def arginit():
     
     return parser.parse_args()
 
+def getSettings():
+    with open(__SETTINGS__, "r") as sFile:
+        return j.load(sFile)
+    
+def saveSettings(json):
+    with open(__SETTINGS__, "w") as sFile:
+        j.dump(json, sFile)
+    
+def getCookie():
+    try:
+        req = r.get(__INDEXURL__)
+        req.raise_for_status()
+        return req.cookies["CT_WIIMM_DE_SESSION24"]
+    
+    except r.exceptions.HTTPError:
+        print("!")
+        exit(1)
+        
+    except r.exceptions.ConnectionError:
+        print("!")
+        exit(1)
+
 def main():
     args = arginit()
     if args.interactive:
-        print("WCTA Browser v{}".format(__VERSION__))
-        print("This software is licensed under the MIT License.")
+        print("WCTA Browser v{} ---".format(__VERSION__))
+        print("This software is distributed under the MIT License (MIT).")
+        
+    sJson = getSettings()
+    if sJson["cookie"] == "" or sJson["expiry"] < time.time():
+        cookie = getCookie()
+        sJson["cookie"] = cookie
+        sJson["expiry"] = time.time() + 2592000
+        saveSettings(sJson)
+    
 
 if __name__ == "__main__":
     main()
